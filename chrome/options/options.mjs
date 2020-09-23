76bd4c965179ef1aa5067b9f1cf3f7ca6bff9061
Cookie Engineer <cookiengineer@protonmail.ch>
2020-09-23 20:06:20 +0200

    :bug: Fixes

--- chrome/options/options.mjs
+++ chrome/options/options.mjs
@@ -3,6 +3,27 @@ import { CACHE, Storage } from '../shared/Storage.mjs';
 
 
 
+const render = (board) => {
+
+	let element = document.createElement('li');
+
+	element.setAttribute('data-organization', board.organization);
+	element.setAttribute('data-repository',   board.repository);
+
+	element.innerHTML = [
+		board.organization + '/' + board.repository + ': ',
+		'<a target="_blank" href="https://github.com/' + board.organization + '/' + board.repository + '/issues">online</a>',
+		' ',
+		'<a target="_blank" href="' + chrome.runtime.getURL('offline/index.html') + '?id=' + board.organization + '/' + board.repository + '">offline</a>',
+		' with ' + board.issues.length + ' issues '
+	].join('');
+
+	return element;
+
+};
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
 	const isArray  = (val) => Object.prototype.toString.call(val) === '[object Array]';
@@ -10,31 +31,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const document = window.document;
 	const storage  = new Storage(chrome);
-	const ELEMENTS = {
+	const elements = {
 		username: document.querySelector('input#username'),
 		token:    document.querySelector('input#token'),
 		boards:   document.querySelector('ul#boards'),
-		save:     document.querySelector('button#save'),
-		sync:     document.querySelector('button#sync')
+		save:     document.querySelector('button#save')
 	};
 
 
 
-	ELEMENTS.save.onclick = () => {
+	elements.save.onclick = () => {
 
 		storage.save({
-			username: ELEMENTS.username.value.trim(),
-			token:    ELEMENTS.token.value.trim(),
+			username: elements.username.value.trim(),
+			token:    elements.token.value.trim(),
 			boards:   CACHE.boards
 		});
-
-	};
-
-	ELEMENTS.sync.onclick = () => {
-
-		if (window.navigator.onLine === true) {
-			// TODO: Synchronize all issues
-		}
 
 	};
 
@@ -43,11 +55,11 @@ window.addEventListener('DOMContentLoaded', () => {
 		if (data !== null) {
 
 			if (isString(data.username) === true) {
-				ELEMENTS.username.value = data.username;
+				elements.username.value = data.username;
 			}
 
 			if (isString(data.token) === true) {
-				ELEMENTS.token.value = data.token;
+				elements.token.value = data.token;
 			}
 
 			if (
@@ -55,7 +67,7 @@ window.addEventListener('DOMContentLoaded', () => {
 				&& data.boards.length > 0
 			) {
 
-				Array.from(ELEMENTS.boards.children).forEach((element) => {
+				Array.from(elements.boards.children).forEach((element) => {
 					element.parentNode.removeChild(element);
 				});
 
@@ -66,41 +78,7 @@ window.addEventListener('DOMContentLoaded', () => {
 					if (a.repository   > b.repository)   return  1;
 					return 0;
 				}).forEach((board) => {
-
-					let button  = document.createElement('button');
-					let element = document.createElement('li');
-
-					button.innerHTML = 'Remove';
-
-					button.onclick = () => {
-
-						let found = CACHE.boards.find((b) => b.organization === board.organization && b.repository === board.repository) || null;
-						if (found !== null) {
-							CACHE.boards.splice(CACHE.boards.indexOf(found), 1);
-						}
-
-						element.parentNode.removeChild(element);
-
-					};
-
-					element.setAttribute('data-organization', board.organization);
-					element.setAttribute('data-repository',   board.repository);
-
-					element.innerHTML = [
-						board.organization + '/' + board.repository + ': ',
-						'<a target="_blank" href="https://github.com/' + board.organization + '/' + board.repository + '/issues">online</a>',
-						' ',
-						'<a target="_blank" href="' + chrome.runtime.getURL('offline/index.html') + '?id=' + board.organization + '/' + board.repository + '">offline</a>',
-						' with ' + board.issues.length + ' issues '
-					].join('');
-
-					setTimeout(() => {
-
-						element.appendChild(button);
-						ELEMENTS.boards.appendChild(element);
-
-					}, 0);
-
+					elements.boards.appendChild(render(board));
 				});
 
 			}
